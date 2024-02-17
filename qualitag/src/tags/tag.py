8785b84f4739f3546441368a2a2d75ea822207ab
Felipe Pires dos Santos <felipepires725@gmail.com>
2024-02-17 22:52:54 +0100

    Create a 'tag' class to handle software's tags

---
+++ qualitag/src/tags/tag.py
@@ -0,0 +1,13 @@
+from attr import define, field
+from attr.validators import instance_of, optional
+
+
+@define
+class Tag:
+
+    name: str = field(validator=instance_of(str), eq=str.lower)
+    bg_color: str = field(validator=instance_of(str), eq=False)
+    fg_color: str = field(validator=instance_of(str), eq=False)
+    description: str = field(
+        default=None, validator=optional(instance_of(str)), eq=False
+    )
