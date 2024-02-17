edcbf7bf53d2cd1ae771503e5fc860751bc50b50
Felipe Pires dos Santos <felipepires725@gmail.com>
2024-02-17 20:10:51 +0100

    First commit

---
+++ setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+with open("README.md", encoding="utf-8") as file:
+    readme = file.read()
+
+with open("LICENSE", encoding="utf-8") as file:
+    _license = file.read()
+
+setup(
+    name="QualiTag",
+    version="0.1.0",
+    description="",
+    long_description=readme,
+    author="Felipe Pires dos Santos",
+    author_email="felipepires725@gmail.com",
+    url="",
+    license=_license,
+    packages=find_packages(exclude=("tests", "docs")),
+)
