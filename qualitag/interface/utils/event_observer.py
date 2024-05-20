58ea3fc09ba716a748e80b7b52fd823db06d73cb
PiresFelipe <felipepires725@gmail.com>
2024-05-20 11:00:45 -0300

    chore: Refactor event observer code for clarity and documentation

--- qualitag/interface/utils/event_observer.py
+++ qualitag/interface/utils/event_observer.py
@@ -4,15 +4,51 @@ from abc import ABC, abstractmethod
 class Event(ABC):
     @abstractmethod
     def attach(self, observer: Observer):
-        pass
+            """
+            Attach an observer to the subject.
+
+            Parameters
+            ----------
+            observer : Observer
+                The observer to be attached.
+
+            Returns
+            -------
+            None
+            """
+            pass
     
     @abstractmethod
     def detach(self, observer: Observer):
-        pass
+            """
+            Detaches an observer from the subject.
+
+            Parameters
+            ----------
+            observer : Observer
+                The observer to be detached.
+
+            Returns
+            -------
+            None
+            """
+            pass
     
     @abstractmethod
     def notify(self):
-        pass
+            """
+            Notify the observer about an event.
+
+            This method is called to notify the observer about an event that has occurred.
+            Implementations of this method should handle the event accordingly.
+
+            Parameters:
+                self (EventObserver): The current instance of the EventObserver class.
+
+            Returns:
+                None
+            """
+            pass
 
 class Observer(ABC):
     @abstractmethod
