e2e8fe151b003568e96c673e28fe0e2ed8277b98
PiresFelipe <felipepires725@gmail.com>
2024-05-20 13:53:03 -0300

    feat: Add observer pattern to `CodingBox` widget for tag events

--- qualitag/interface/widgets/shared/coding_box.py
+++ qualitag/interface/widgets/shared/coding_box.py
@@ -1,14 +1,23 @@
 import customtkinter as ctk
 from tkinter import TclError
+from utils import Observer
+from widgets.tags import TagsManager
 
 
-class CodingBox(ctk.CTkTextbox):
+class CodingBox(ctk.CTkTextbox, Observer):
 
-    def __init__(self, master, **kwargs):
+    def __init__(self, master, tags_manager: TagsManager, **kwargs):
+        assert isinstance(
+            tags_manager, TagsManager
+        ), "tags_manager must be an instance of TagsManager"
+
         super().__init__(master, **kwargs)
 
+        # Subcribing to tags_manager events
+        tags_manager.events.attach(self)
+
         self.__build()
-        
+
         # Binding selection events
         self.bind("<B1-Motion> <ButtonRelease-1>", self.on_select)
         self.bind("<Double-Button-1>", self.on_select)
@@ -16,16 +25,36 @@ class CodingBox(ctk.CTkTextbox):
     def __build(self):
         pass
 
-    def on_select(self, event):
-        
-        def get_selection():
-            try:
-                selection = self.index("sel.first")
-                print("on_select ", event, selection)
-            except TclError:
-                return
-            
-        self.after(3, get_selection)
+    def get_selection(self, as_index=False) -> tuple[str, str] | str | None:
+        """
+        Get the selected text or indices in the coding box.
 
-    def add_tag(self, event):
-        print(event)   
+        Parameters:
+        ---
+            as_index (bool, optional): If True, returns the indices of the selected text.
+                If False, returns the selected text. Defaults to False.
+
+        Returns:
+        ---
+            tuple[str, str] | str | None: If as_index is True, returns a tuple of the indices
+                of the selected text. If as_index is False, returns the selected text.
+                If no text is selected, returns None.
+        """
+        try:
+            if as_index:
+                return self.index("sel.first"), self.index("sel.last")
+            return self.get("sel.first", "sel.last")
+        except TclError:
+            return None
+
+    def on_select(self, event):
+
+        self.after(3, self.get_selection)
+
+    def on_event(self, event):
+
+        if self.winfo_exists():
+            selection = self.get_selection(as_index=True)
+            if selection is not None:
+                print(selection)
+
