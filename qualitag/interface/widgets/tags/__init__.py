a2cd44dbaf69886366da3d6669a4a40a439d051a
PiresFelipe <felipepires725@gmail.com>
2024-05-23 12:32:22 -0300

    feat: Add `TagView` widget for displaying tags

---
+++ qualitag/interface/widgets/tags/__init__.py
@@ -0,0 +1,4 @@
+from .tag_creator import TagCreator
+from .tag_view import TagView
+
+__all__ = ["TagCreator", "TagView"]
--- qualitag/interface/widgets/tags/tag_view.py
+++ qualitag/interface/widgets/tags/tag_view.py
@@ -1,54 +1,21 @@
-from tkinter import StringVar
-from ttkbootstrap.colorutils import update_hsl_value
-from ttkbootstrap.tooltip import ToolTip
-from customtkinter import CTkFrame, CTkLabel
+from customtkinter import CTkLabel, CTk
+from colour import Color
+
+class Tag:
+    name = "Test"
+    description = "This is a test tag"
+    base_color = "#FF0000"
 
 
-class TagView(CTkFrame):
-
-    def __init__(
-        self,
-        *args,
-        width: int = 500,
-        height: int = 500,
-        tag: dict[str, str | StringVar],
-        **kwargs
-    ):
+class TagView(CTkLabel):
+    def __init__(self, *args, tag: Tag, width: int = 100, height: int = 32, **kwargs):
         super().__init__(*args, width=width, height=height, **kwargs)
-
-        # self.__init_vars(tag)
-        # self.__build()
-
-    def __init_vars(self, tag: dict[str, str | StringVar]):
-        if isinstance(tag["name"], StringVar):
-            self.name = tag["name"]
-        else:
-            self.name = StringVar(self, tag["name"])
-
-        if isinstance(tag["description"], StringVar):
-            self.description = tag["description"]
-        else:
-            self.description = StringVar(self, tag["description"])
-
-        if isinstance(tag["color"], StringVar):
-            self.base_color = tag["color"]
-        else:
-            self.base_color = StringVar(self, tag["color"])
-
+        
+        self.__tag = tag
         self.__bg = None
         self.__fg = None
-
-    def __build(self):
-        CTkLabel(self, textvariable=self.name, text_color=self.foreground).pack(
-            side="left", fill="both"
-        )
-
-        tooltip_text = self.description.get()
-        if tooltip_text and len(tooltip_text) > 0:
-            tooltip_text = (
-                tooltip_text if len(tooltip_text) < 150 else tooltip_text[:100] + "..."
-            )
-            ToolTip(self, tooltip_text, delay=0.5)
+        
+        self.configure(corner_radius=height, fg_color=self.background, text_color=self.foreground, text=self.__tag.name)
 
     @property
     def background(self):
@@ -56,12 +23,12 @@ class TagView(CTkFrame):
         Returns the background color of the tag view.
 
         If the background color has not been set, it calculates a new color based on the base color
-        by adjusting the luminance to 0.75.
+        by adjusting the luminance to 0.9.
         """
         if self.__bg is None:
-            self.__bg = update_hsl_value(
-                self.base_color, lum=0.75, inmodel="hex", outmodel="hex"
-            )
+            self.__bg = Color(self.__tag.base_color)
+            self.__bg.set_luminance(0.9)
+            self.__bg = self.__bg.get_hex_l()
         return self.__bg
 
     @property
@@ -73,24 +40,17 @@ class TagView(CTkFrame):
         The foreground color is calculated by updating the HSL value of the base color with a luminance of 0.25.
         """
         if self.__fg is None:
-            self.__fg = update_hsl_value(
-                self.base_color, lum=0.25, inmodel="hex", outmodel="hex"
-            )
+            self.__fg = Color(self.__tag.base_color)
+            self.__fg.set_luminance(0.25)
+            self.__fg = self.__fg.get_hex_l()
         return self.__fg
 
 
 if __name__ == "__main__":
-    from customtkinter import CTk
-
     root = CTk()
-
-    tag = {
-        "name": "Tag Name",
-        "description": "This is a tag description",
-        "color": "#ff0000",
-    }
-
-    tag_view = TagView(root, tag=tag)
-    tag_view.pack()
-
+    tg = TagView(
+        root,
+        tag = Tag(),
+    )
+    tg.pack()
     root.mainloop()
