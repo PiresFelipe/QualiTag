995e5db0c7b864377f80597f81598130c60f9cc7
PiresFelipe <felipepires725@gmail.com>
2024-05-24 10:39:09 -0300

    chore: Refactor `TagView` widget for improved readability and maintainability

--- qualitag/interface/widgets/tags/tag_view.py
+++ qualitag/interface/widgets/tags/tag_view.py
@@ -1,21 +1,21 @@
 from customtkinter import CTkLabel, CTk
 from colour import Color
 
-class Tag:
-    name = "Test"
-    description = "This is a test tag"
-    base_color = "#FF0000"
-
 
 class TagView(CTkLabel):
-    def __init__(self, *args, tag: Tag, width: int = 100, height: int = 32, **kwargs):
+    def __init__(self, *args, tag, width: int = 100, height: int = 32, **kwargs):
         super().__init__(*args, width=width, height=height, **kwargs)
-        
+
         self.__tag = tag
         self.__bg = None
         self.__fg = None
-        
-        self.configure(corner_radius=height, fg_color=self.background, text_color=self.foreground, text=self.__tag.name)
+
+        self.configure(
+            corner_radius=height,
+            fg_color=self.background,
+            text_color=self.foreground,
+            text=self.__tag.name,
+        )
 
     @property
     def background(self):
@@ -36,21 +36,13 @@ class TagView(CTkLabel):
         """
         Returns the foreground color of the tag view.
 
-        If the foreground color is not set, it calculates the foreground color based on the base color.
-        The foreground color is calculated by updating the HSL value of the base color with a luminance of 0.25.
+        If the foreground color is not set, it calculates the foreground
+        color based on the base color.
+        The foreground color is calculated by updating the HSL value
+        of the base color with a luminance of 0.25.
         """
         if self.__fg is None:
             self.__fg = Color(self.__tag.base_color)
             self.__fg.set_luminance(0.25)
             self.__fg = self.__fg.get_hex_l()
         return self.__fg
-
-
-if __name__ == "__main__":
-    root = CTk()
-    tg = TagView(
-        root,
-        tag = Tag(),
-    )
-    tg.pack()
-    root.mainloop()
