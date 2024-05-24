c2213a510f720b2834639dc4510bf6dc1f32d499
PiresFelipe <felipepires725@gmail.com>
2024-05-24 15:39:48 -0300

    Refactor `TagEvent` class to improve code readability and maintainability

--- qualitag/interface/widgets/tags/tags_events.py
+++ qualitag/interface/widgets/tags/tags_events.py
@@ -1,31 +1,32 @@
 from utils import Event, Observer
 
+
 class TagEvent(Event):
     __observers: set[Observer] = set()
-    __state: dict[str, str] | None = None
-    
+    __state = None
+
     @property
     def state(self) -> dict[str, str] | None:
         return self.__state
-    
+
     def attach(self, observer: Observer):
         self.__observers.add(observer)
-    
+
     def detach(self, observer: Observer):
         self.__observers.remove(observer)
-    
+
     def notify(self):
         if self.__state is not None:
             for observer in self.__observers:
                 observer.on_event(self)
-    
+
     def generate_event(self, tag):
         """
         Generate an event for the given tag.
 
         Parameters
         ----------
-        tag : str
+        tag : TagView
             The tag to generate an event for.
         """
         if self.__state is None:
