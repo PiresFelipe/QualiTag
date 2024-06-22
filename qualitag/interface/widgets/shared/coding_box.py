57bea7c4e7a798b64a21c670cdc52e2a5d572a57
PiresFelipe <felipepires725@gmail.com>
2024-06-22 17:22:11 -0300

    refactor: Update `coding_box.py` to set text in the coding box

--- qualitag/interface/widgets/shared/coding_box.py
+++ qualitag/interface/widgets/shared/coding_box.py
@@ -21,11 +21,21 @@ class CodingBox(ctk.CTkTextbox, Observer):
 
         # Subcribing to tags_manager events
         events.attach(self)
+        
+        self.set_text("lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua")
 
-        self.insert("1.0", "Lorem ipsum dolor sit amet, consectetur adipiscing elit.")
+    def set_text(self, text: str):
+        """
+        Set the text of the coding box.
 
-    def __reset_debounce(self):
-        self.__debounce = None
+        Parameters:
+        ---
+            text (str): Text to be set in the coding box.
+        """
+        self.configure(state="normal")
+        self.delete("1.0", "end")
+        self.insert("1.0", text)
+        self.configure(state="disabled")
 
     def get_selection(self, as_index=False) -> tuple[str, str] | str | None:
         """
@@ -49,7 +59,7 @@ class CodingBox(ctk.CTkTextbox, Observer):
         except TclError:
             return None
 
-    def get_codes(self) -> dict[str, list[str]]:
+    def get_codes(self):
         """
         Get the tags in this text and texts associated.
 
@@ -59,9 +69,12 @@ class CodingBox(ctk.CTkTextbox, Observer):
         """
         tags = self.tag_names()
         codes = {}
-
         for tag in tags:
-            pass
+            if tag == "sel":
+                continue
+            codes[tag] = self.tag_ranges(tag)
+        print(codes)   
+        return codes
 
     def on_event(self, event):
         if self.winfo_exists() and event.event_type == "clicked":
@@ -78,6 +91,7 @@ class CodingBox(ctk.CTkTextbox, Observer):
                     "<Button-3>",
                     lambda e: self.remove_tag(e, event.tag.full_name),
                 )
+                self.tag_remove("sel", "sel.first", "sel.last")
                 self.tag_raise("sel")
 
     def remove_tag(self, event: Event, tag_name: str):
@@ -88,7 +102,6 @@ class CodingBox(ctk.CTkTextbox, Observer):
         tag_indices = self.tag_prevrange(tag_name, index)
         if len(tag_indices) == 0:
             tag_indices = self.tag_nextrange(tag_name, index)
-        print(tag_indices)
 
         if self.__debounce is not None:
             self.after_cancel(self.__debounce)
