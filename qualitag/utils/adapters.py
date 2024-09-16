1726f85edb3f34de6c684fb2c7274bcb816e5052
PiresFelipe <felipepires725@gmail.com>
2024-09-16 00:53:01 -0300

    refactor: Fix typo in convert_char_idx_to_lines function

--- qualitag/utils/adapters.py
+++ qualitag/utils/adapters.py
@@ -1,5 +1,5 @@
 # Interchangeable adapter for the different ways to assign a tag.
-def convert_lines_to_char_idx(text_box, line_info:str):
+def convert_lines_to_char_idx(text_box, line_info: str):
     line, char = map(int, text_box.index(line_info).split("."))
     char_index = 0
     for i in range(line - 1):
@@ -9,7 +9,8 @@ def convert_lines_to_char_idx(text_box, line_info:str):
     char_index += char
     return char_index
 
-def covert_char_idx_to_lines(text:str, char_index:int):
+
+def covert_char_idx_to_lines(text: str, char_index: int):
     lines = text.split("\n")
     line_index = 1
     char_count = 0
@@ -17,7 +18,6 @@ def covert_char_idx_to_lines(text:str, char_index:int):
         char_count += len(line) + 1  # +1 for line break
         if char_count > char_index:
             char_index_in_line = char_index - (char_count - len(line))
-            return f"{line_index}.{char_index_in_line}"
+            return f"{line_index}.{char_index_in_line + 1}"
         line_index += 1
     return ""
-    
